85c156f91af910aaaa3074892e8b8f23d74a2974
Sean Aubin <saubin@uwaterloo.ca>
2014-11-04 11:30:12 -0500

    get most recent link

--- make-usb.py
+++ make-usb.py
@@ -4,6 +4,9 @@
 
 import os
 import urllib, urlparse
+import re
+
+from bs4 import BeautifulSoup
 
 # Because I want pretty dict initialization
 class Vividict(dict):
@@ -12,18 +15,20 @@ class Vividict(dict):
         return value
 
 # TODO: Figure out how to just get the most recent version and then dynamically
-# create the batch file and shell file
-# Where the get the various packages
-anaconda_list = Vividict()
-anaconda_list["os_x"]["64_bit"] = "http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-MacOSX-x86_64.pkg"
-anaconda_list["linux"]["32_bit"] = "http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86.sh"
-anaconda_list["linux"]["64_bit"] = "http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh"
-anaconda_list["windows"]["32_bit"] = "http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Windows-x86.exe"
-anaconda_list["windows"]["64_bit"] = "http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Windows-x86_64.exe"
+# create the batch file and shell file 
+# Use http://repo.continuum.io/anaconda3/ if you want Python3, but for now we're going to use Python2, because I haven't converted the GUI to be Python2 compatible
+anaconda_url = "http://repo.continuum.io/archive/"
+soup = BeautifulSoup(urllib.urlopen(anaconda_url).read())
+most_recent_version = re.search(r"[0-9]+\.[0-9]+\.[0-9]+", soup.find_all('a')[-1].text).group(0)
 
-packages_list = Vividict()
-#packages_list["linux"]
-#packages_list = ["numpy", "ipython notebook", "Tornado"]
+# Where to get the various packages
+anaconda_list = Vividict()
+anaconda_list["os_x"]["64_bit"] = ("%sAnaconda-%s-MacOSX-x86_64.pkg" %(anaconda_url, most_recent_version))
+anaconda_list["linux"]["32_bit"] = ("%sAnaconda-%s-Linux-x86.sh" %(anaconda_url, most_recent_version))
+anaconda_list["linux"]["64_bit"] = ("%sAnaconda-%s-Linux-x86_64.sh" %(anaconda_url, most_recent_version))
+anaconda_list["windows"]["32_bit"] = ("%sAnaconda-%s-Windows-x86.exe" %(anaconda_url, most_recent_version))
+anaconda_list["windows"]["64_bit"] = ("%sAnaconda-%s-Windows-x86_64.exe" %(anaconda_url, most_recent_version))
+
 
 # Make directories and download packages
 for op_sys, op_key in anaconda_list.iteritems():
