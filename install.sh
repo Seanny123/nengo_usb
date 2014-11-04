b056f66aaa04f85c56f5b9dbf8dd8d0d5163bd7b
Sean Aubin <saubin@uwaterloo.ca>
2014-11-04 14:48:48 -0500

    got flow control right

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
+# I'm going to assume that if you're using this script, you're installing offline. Otherwise, you should just be following the README instructions.
 function usage()
 {
 	echo "Install nengo and it's requirements"
 	echo ""
 	echo "-h --help"
-	echo "-o offline install"
 	echo "-f force reinstall"
 	echo ""
 }
@@ -11,20 +11,28 @@ function usage()
 # All installation will be done in the current directory of the script, unless an argument is passed
 if [ -z "$1" ]; then
 	# taken from https://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
-	dir = "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 else
-	dir = "$1"
+	dir="$1"
 fi
 
 # If nengo is not installed, install Anaconda depending on version so that
 # the dependencies can be installed. This should be able to proceed without 
 # replacing the python distribution?
-result = "$(python -c 'import nengo')"
+result="$(python -c 'import nengo')"
 if[$? -ne 0 || force_install -eq True]; then
 	if["$(uname -m)" == "x86_64"]; then
-		arch = "64_bit"
+		arch="64_bit"
 	else
-		arch = "32_bit"
+		arch="32_bit"
+	fi
+	result="$(python -V)"
+	# if python is installed, then just install miniconda and requirements
+	# so that the user's install isn't over-written
+	if[$? -ne 0]; then
+		dist="Miniconda"
+	else
+		dist="Anaconda"
 	fi
 
 	if [ "$(uname)" == "Darwin" ]; then
@@ -34,9 +42,11 @@ if[$? -ne 0 || force_install -eq True]; then
 	elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
 		echo "This is not the script for running in Windows. Use the batch file"
 	fi
-fi
 
-# if the user checked the offline install option
+	if[dist -eq "Miniconda"]; then
+		# check which requirements aren't met and install them
+	fi
+fi
 
 
 # Install Nengo
